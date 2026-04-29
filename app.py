d0106418cffad3819a6ac160571b491e482a4c85
babacar-qwerty <babacarpsn@gmail.com>
2026-04-29 09:00:37 +0000

    J'ai mis les links de la navbar en flex-wrap: wrap, J'ai ajusté les les margin de titre de la section acceuil, Ajout des request dans la partie flask pour récuperer les données du formulaires mais je n'ai pas encore exploité les données.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
@@ -22,9 +22,17 @@ def cosmetique():
 def recettes():
     return render_template('recettes.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET','POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        nom = request.form.get('nom')
+        email = request.form.get('email')
+        telephone = request.form.get('telephone')
+        sujet = request.form.get('sujet')
+        message = request.form.get('message')
+        return f"Merci {nom} pour votre message !"
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
