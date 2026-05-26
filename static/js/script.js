8da18dbc0bcfc7559a3017fbe84739f132c49ad3
babacar-qwerty <babacarpsn@gmail.com>
2026-05-26 15:24:14 +0000

    Ajout d'un effet sur la navbar au scroll, titre Accueil effacé.

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,19 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('open');
 
 })
+
+const nav = document.querySelector('nav')
+
+// Effet de scroll sur la navbar
+window.addEventListener("scroll", ()=>{
+    if (window.scrollY >= 150){
+        nav.style.top = "-200px";//quand on swipe ça cache
+        if (links_container.classList.contains("open") && hamburger.classList.contains("open")){
+            //si le menu ham était ouvert, au scroll il se ferme
+            links_container.classList.remove('open');
+             hamburger.classList.remove('open');           
+        }
+    }else{
+        nav.style.top = 0;
+    }
+})
